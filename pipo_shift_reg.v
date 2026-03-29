d3fbc2a09c4e0b85384385a9b53c4092d98763d4
mallala1028-dotcom <mallala.1028@gmail.com>
2026-03-29 10:43:22 +0530

    Update pipo_shift_reg.v

--- pipo_shift_reg.v
+++ pipo_shift_reg.v
@@ -10,19 +10,4 @@ end
 endmodule
 
 
-//tb_shift_reg//
-module tb_pi_po_reg;
-reg clk,rst;
-reg [3:0]din;
-wire [3:0]dout;
-pi_po_reg btt(.*);
-always #50 clk=~clk;
-initial begin
-$monitor("clk=%b,rst=%b,din=%b,dout=%b",clk,rst,din,dout);
-clk=1'b0;
-rst=1'b1;
-din=4'b1101;
-#100 rst=1'b0;din=4'b1101;
-#500 rst=1'b0;din=4'b1010;
-end
-endmodule
+
